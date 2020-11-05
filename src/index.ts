77edff39debaccb418fe46fd82997e6a930abe91
somin <qkrthals0524@naver.com>
2020-11-05 16:40:46 +0900

    Classes

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-interface Human {
-    name:string;
-    age:number;
-    gender:string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender : string;
+    constructor(name:string,age:number,gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
-const person ={
-    name :"somin",
-    age:24,
-    gender:"female"
-}
-
+const somin = new Human("somin",20,"female");
 
 
 const sayHi = (person:Human):string => {
     return(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`);
 };
-console.log(sayHi(person));
+console.log(sayHi(somin));
 
 export{};
