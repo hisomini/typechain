92340a2708b8257f3de260c7e7c4e38ae8c6b24c
somin <qkrthals0524@naver.com>
2020-11-06 20:53:16 +0900

    #3 Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { hasUncaughtExceptionCaptureCallback } from "process";
+import * as CryptoJs from "crypto-js";
 
 class Block {
     public index: number;
@@ -6,6 +6,14 @@ class Block {
     public previousHash: string;
     public data : string;
     public timestamp :number;
+
+    static calculateBlockHash =(
+        index:number,
+        previousHash:string,
+        timestamp:number,
+        data:string
+    ): string => CryptoJs.SHA256(index + previousHash+ timestamp + data).toString();
+
     constructor (
         index: number,
         hash: string,
@@ -23,6 +31,30 @@ class Block {
 const genesisBlock: Block = new Block(0,"20202020","","hell",12345);
 let blockchain : [Block] = [genesisBlock];
 
-console.log(blockchain)
+const getBlockchain = (): Block[] => blockchain;
 
+const getLatestBlock = (): Block => blockchain[blockchain.length -1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() /1000);
+
+const createNewBlock = (data:string): Block =>{
+    const previousBlock: Block = getLatestBlock();
+    const newIndex:number = previousBlock.index +1;
+    const newTimestamp:number = getNewTimeStamp();
+    const newHash:string = Block.calculateBlockHash(
+        newIndex,
+        previousBlock.hash,
+        newTimestamp,
+        data
+    );
+    const newBlock: Block = new Block(
+        newIndex,
+        newHash,
+        previousBlock.hash,
+        data,
+        newTimestamp
+    );
+    return newBlock;
+}
+console.log(createNewBlock("hello"), createNewBlock("bye"))
 export{};
